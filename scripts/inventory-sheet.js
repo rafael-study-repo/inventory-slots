ee77f2ce2821bf99ae6b124ccf37b297b2bce5ba
Rafael Eduardo <study-mdn-rafael@outlook.com>
2025-08-22 01:41:10 -0300

    --amend

--- scripts/inventory-sheet.js
+++ scripts/inventory-sheet.js
@@ -6,26 +6,17 @@ export class InventorySlots5eSheet extends dnd5e.applications
     options.template = "modules/inventory-slots/template/actor-sheet.html";
     return options;
   }
-
   async getData(options) {
     const data = await super.getData(options);
-
     const strength = data.actor.system.abilities.str.value;
     const maxSlots = strength + 5;
-
     const items = data.items.filter((i) => i.type === "equipment");
     let usedSlots = 0;
-
     for (const item of items) {
       item.slotSize = item.flags["inventory-slots"]?.slotSize || 1;
       usedSlots += item.slotSize;
     }
-
-    data.inventorySlots = {
-      used: usedSlots,
-      max: maxSlots,
-    };
-
+    data.inventorySlots = { used: usedSlots, max: maxSlots };
     return data;
   }
 }
--- scripts/module.js
+++ scripts/module.js
@@ -1,12 +1,9 @@
 import { InventorySlots5eSheet } from "./inventory-sheet.js";
-
 Hooks.once("init", () => {
-  console.log("Inventory Slots | Inicializando...");
-
-  Actors.unregisterSheet("dnd5e", "core.ActorSheet5eCharacter");
+  console.log("Inventory Slots | Inicializando..."); // Registrar nova ficha para personagens
   Actors.registerSheet("dnd5e", InventorySlots5eSheet, {
     types: ["character"],
     label: "Inventory Slots (Mausritter-style)",
-    makeDefault: true,
+    makeDefault: false,
   });
 });
